82049531a85233b56eca09feba7b6f928720da47
PegahNa <pegah_uk@yahoo.co.uk>
2023-08-10 15:33:33 +0100

    main file in progress

--- activity-recommender/main.py
+++ activity-recommender/main.py
@@ -1,9 +1,13 @@
 import json
-
+# Load locations data from JSON file
 def load_locations_data():
     with open('data/locations.json') as json_file:
         data = json.load(json_file)
     return data
+
+# Search activities by location
+def search
+
 
 def main():
     locations_data = load_locations_data()
@@ -19,8 +23,8 @@ def main():
             print("Visual Accessibility:", activity['visual_accessibility'])
             print("Opening Hours:", activity['opening_hours'])
             print("Current Opening Hours:", activity['current_opening_hours'])
-            print()  # Print a newline for better readability
+            print() 
 
+            
 if __name__ == "__main__":
     main()
-    
