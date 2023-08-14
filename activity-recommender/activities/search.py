fa3aa76bf5fefc90ed167327fe10654d79d2b294
Fabiana <fabiana.magali@gmail.com>
2023-08-14 13:09:16 +0100

    Testing for search.py in progress

--- activity-recommender/activities/search.py
+++ activity-recommender/activities/search.py
@@ -14,64 +14,40 @@ zero_stars = [0.0 + (x * 0.1)
              for x in range(0, 10)]
 
 
+class Filter:
+    def __init__(self, city):
+        self.city = city
 
-# This is just to test my function, it'll be deleted later
-with open('C:/Users/Fabiana/Downloads/CFG degree/Group-1-activity-recommender/data/locations.json') as json_file:
-    fake_data = json.load(json_file)
+    def filter_by_price(self, target_price):
+        result = [item for item in self.city if item["price"] in target_price]
+        pretty_result = json.dumps(result, indent=4)
+        return pretty_result
 
+    def filter_by_rating(self, target_rating):
+        result = [item for item in self.city if item["rating"] in target_rating]
+        pretty_result = json.dumps(result, indent=4)
+        return pretty_result
 
-    class Filter:
-        def __init__(self, city):
-            self.city = city
+    def filter_by_wheelchair_accessible_entrance(self):
+        result = [item for item in self.city if item["wheelchair_accessible_entrance"] == True]
+        pretty_result = json.dumps(result, indent=4)
+        print("wheelchair_accessible_entrance")
+        return pretty_result
 
-        def filter_by_price(self, target_price):
-            result = [item for item in self.city if item["price"] in target_price]
-            pretty_result = json.dumps(result, indent=4)
-            return pretty_result
+    def filter_by_hearing_accessibility(self):
+        result = [item for item in self.city if item["hearing_accessibility"] == True]
+        pretty_result = json.dumps(result, indent=4)
+        print("hearing_accessibility")
+        return pretty_result
 
-        def filter_by_rating(self, target_rating):
-            result = [item for item in self.city if item["rating"] in target_rating]
-            pretty_result = json.dumps(result, indent=4)
-            return pretty_result
+    def filter_by_visual_accessibility(self):
+        result = [item for item in self.city if item["visual_accessibility"] == True]
+        pretty_result = json.dumps(result, indent=4)
+        print("visual_accessibility")
+        return pretty_result
 
-        def filter_by_wheelchair_accessible_entrance(self):
-            result = [item for item in self.city if item["wheelchair_accessible_entrance"] == True]
-            pretty_result = json.dumps(result, indent=4)
-            print("wheelchair_accessible_entrance")
-            return pretty_result
-
-        def filter_by_hearing_accessibility(self):
-            result = [item for item in self.city if item["hearing_accessibility"] == True]
-            pretty_result = json.dumps(result, indent=4)
-            print("hearing_accessibility")
-            return pretty_result
-
-        def filter_by_visual_accessibility(self):
-            result = [item for item in self.city if item["visual_accessibility"] == True]
-            pretty_result = json.dumps(result, indent=4)
-            print("visual_accessibility")
-            return pretty_result
-
-        def filter_by_current_opening_hours(self):
-            result = [item for item in self.city if item["current_opening_hours"] == "open_now"]
-            pretty_result = json.dumps(result, indent=4)
-            print("current_opening_hours")
-            return pretty_result
-
-
-    # Testing the filters
-    print("Madrid")
-    madrid = Filter(fake_data["spain"])
-    print(madrid.filter_by_price(range(2,10)))
-    print(madrid.filter_by_price(range(12, 20)))
-    print(madrid.filter_by_price(range(20, 50)))
-    print(madrid.filter_by_rating(four_stars))
-    print(madrid.filter_by_rating(three_stars))
-    print(madrid.filter_by_rating(two_stars))
-    print(madrid.filter_by_rating(five_stars))
-
-    print("Paris")
-    paris = Filter(fake_data["paris"])
-    print(paris.filter_by_price(range(12,20)))
-    print(paris.filter_by_wheelchair_accessible_entrance())
-    print(paris.filter_by_hearing_accessibility())
+    def filter_by_current_opening_hours(self):
+        result = [item for item in self.city if item["current_opening_hours"] == "open_now"]
+        pretty_result = json.dumps(result, indent=4)
+        print("current_opening_hours")
+        return pretty_result
---
+++ activity-recommender/tests/test_search.py
@@ -0,0 +1,355 @@
+import json
+import unittest
+from ..activities.search import Filter
+
+
+class TestFilter(unittest.TestCase):
+    # It'll run its code before every single test
+    def setUp(self):
+        # It'll mock the data from all the cities in the JSON file
+        self.spain_data = [
+            {
+                "activity": "Plaza Mayor",
+                "price": 0,
+                "rating": 4.6,
+                "wheelchair_accessible_entrance": True,
+                "hearing_accessibility": False,
+                "visual_accessibility": False,
+                "opening_hours": "24hs",
+                "current_opening_hours": "open_now"
+            },
+            {
+                "activity": "El Retiro Park",
+                "price": 0,
+                "rating": 4.8,
+                "wheelchair_accessible_entrance": True,
+                "hearing_accessibility": False,
+                "visual_accessibility": False,
+                "opening_hours": "24hs",
+                "current_opening_hours": "open_now"
+            },
+            {
+                "activity": "Museo nacional del Prado",
+                "price": 15,
+                "rating": 4.7,
+                "wheelchair_accessible_entrance": True,
+                "hearing_accessibility": True,
+                "visual_accessibility": True,
+                "opening_hours": "mon-sat 10:00-20:00, sun 10:00-19:00",
+                "current_opening_hours": "open_now"
+            },
+            {
+                "activity": "Royal Palace of Madrid",
+                "price": 12,
+                "rating": 4.6,
+                "wheelchair_accessible_entrance": True,
+                "hearing_accessibility": True,
+                "visual_accessibility": True,
+                "opening_hours": "mon-sat 10:00-19:00, sun 10:00-16:00",
+                "current_opening_hours": "open_now"
+            },
+            {
+                "activity": "Santiago Bernabeu Stadium",
+                "price": 15,
+                "rating": 4.6,
+                "wheelchair_accessible_entrance": True,
+                "hearing_accessibility": False,
+                "visual_accessibility": False,
+                "opening_hours": "mon-sat 9:30-19:00, sun 10:00-18:30",
+                "current_opening_hours": "open_now"
+            },
+            {
+                "activity": "Mercado de San Miguel",
+                "price": 0,
+                "rating": 4.4,
+                "wheelchair_accessible_entrance": True,
+                "hearing_accessibility": False,
+                "visual_accessibility": False,
+                "opening_hours": "sun-thu 10:00-00:00, fri-sat 10:00-01:00",
+                "current_opening_hours": "open_now"
+            },
+            {
+                "activity": "Puerta de Alcala",
+                "price": 0,
+                "rating": 4.7,
+                "wheelchair_accessible_entrance": True,
+                "hearing_accessibility": False,
+                "visual_accessibility": False,
+                "opening_hours": "24hs",
+                "current_opening_hours": "open_now"
+            }
+        ]
+        self.paris_data = [
+            {
+              "activity": "Eiffel Tower",
+              "price": 16,
+              "rating": 4.7,
+              "wheelchair_accessible_entrance": True,
+              "hearing_accessibility": True,
+              "visual_accessibility": True,
+              "opening_hours": "daily 9:00-00:45",
+              "current_opening_hours": "open_now"
+            },
+            {
+              "activity": "Louvre Museum",
+              "price": 15,
+              "rating": 4.8,
+              "wheelchair_accessible_entrance": True,
+              "hearing_accessibility": True,
+              "visual_accessibility": True,
+              "opening_hours": "mon, thu, sat, sun 9:00-18:00, wed, fri 9:00-21:45",
+              "current_opening_hours": "open_now"
+            },
+            {
+              "activity": "Notre-Dame Cathedral",
+              "price": 0,
+              "rating": 4.6,
+              "wheelchair_accessible_entrance": True,
+              "hearing_accessibility": False,
+              "visual_accessibility": False,
+              "opening_hours": "daily 8:00-18:45",
+              "current_opening_hours": "open_now"
+            },
+            {
+              "activity": "Montmartre",
+              "price": 0,
+              "rating": 4.5,
+              "wheelchair_accessible_entrance": False,
+              "hearing_accessibility": False,
+              "visual_accessibility": False,
+              "opening_hours": "24hs",
+              "current_opening_hours": "open_now"
+            },
+            {
+              "activity": "Seine River Cruise",
+              "price": 12,
+              "rating": 4.7,
+              "wheelchair_accessible_entrance": True,
+              "hearing_accessibility": True,
+              "visual_accessibility": True,
+              "opening_hours": "daily 9:00-23:00",
+              "current_opening_hours": "open_now"
+            },
+            {
+              "activity": "Versailles Palace",
+              "price": 20,
+              "rating": 4.6,
+              "wheelchair_accessible_entrance": True,
+              "hearing_accessibility": True,
+              "visual_accessibility": True,
+              "opening_hours": "tue-sun 9:00-18:30",
+              "current_opening_hours": "open_now"
+            },
+            {
+              "activity": "Centre Pompidou",
+              "price": 14,
+              "rating": 4.5,
+              "wheelchair_accessible_entrance": True,
+              "hearing_accessibility": True,
+              "visual_accessibility": True,
+              "opening_hours": "wed-mon 11:00-21:00",
+              "current_opening_hours": "open_now"
+            }]
+
+
+    # VALID CASES
+    def test_filter_by_price_cheap(self):
+        city_filter = Filter(self.spain_data)
+        target_price = range(1, 10)
+
+        result = city_filter.filter_by_price(target_price)
+        expected_result = []
+
+        # Check whether the result is right
+        self.assertEqual(result, json.dumps(expected_result, indent=4))
+
+    def test_filter_by_price_medium(self):
+        city_filter = Filter(self.spain_data)
+        target_price = range(10,20)
+
+        result = city_filter.filter_by_price(target_price)
+        expected_result = [
+    {
+        "activity": "Museo nacional del Prado",
+        "price": 15,
+        "rating": 4.7,
+        "wheelchair_accessible_entrance": True,
+        "hearing_accessibility": True,
+        "visual_accessibility": True,
+        "opening_hours": "mon-sat 10:00-20:00, sun 10:00-19:00",
+        "current_opening_hours": "open_now"
+    },
+    {
+        "activity": "Royal Palace of Madrid",
+        "price": 12,
+        "rating": 4.6,
+        "wheelchair_accessible_entrance": True,
+        "hearing_accessibility": True,
+        "visual_accessibility": True,
+        "opening_hours": "mon-sat 10:00-19:00, sun 10:00-16:00",
+        "current_opening_hours": "open_now"
+    },
+    {
+        "activity": "Santiago Bernabeu Stadium",
+        "price": 15,
+        "rating": 4.6,
+        "wheelchair_accessible_entrance": True,
+        "hearing_accessibility": False,
+        "visual_accessibility": False,
+        "opening_hours": "mon-sat 9:30-19:00, sun 10:00-18:30",
+        "current_opening_hours": "open_now"
+    }
+]
+        # Check whether the result is right
+        self.assertEqual(result, json.dumps(expected_result, indent=4))
+
+    def test_filter_by_price_expensive(self):
+        city_filter = Filter(self.spain_data)
+        target_price = range(20, 100)
+
+        result = city_filter.filter_by_price(target_price)
+        expected_result = []
+        # Check whether the result is right
+        self.assertEqual(result, json.dumps(expected_result, indent=4))
+
+    def test_filter_by_price_free(self):
+        city_filter = Filter(self.spain_data)
+        target_price = [0]
+
+        result = city_filter.filter_by_price(target_price)
+        expected_result = [{
+        "activity": "Plaza Mayor",
+        "price": 0,
+        "rating": 4.6,
+        "wheelchair_accessible_entrance": True,
+        "hearing_accessibility": False,
+        "visual_accessibility": False,
+        "opening_hours": "24hs",
+        "current_opening_hours": "open_now"
+    },
+    {
+        "activity": "El Retiro Park",
+        "price": 0,
+        "rating": 4.8,
+        "wheelchair_accessible_entrance": True,
+        "hearing_accessibility": False,
+        "visual_accessibility": False,
+        "opening_hours": "24hs",
+        "current_opening_hours": "open_now"
+    },
+    {
+        "activity": "Mercado de San Miguel",
+        "price": 0,
+        "rating": 4.4,
+        "wheelchair_accessible_entrance": True,
+        "hearing_accessibility": False,
+        "visual_accessibility": False,
+        "opening_hours": "sun-thu 10:00-00:00, fri-sat 10:00-01:00",
+        "current_opening_hours": "open_now"
+    },
+    {
+        "activity": "Puerta de Alcala",
+        "price": 0,
+        "rating": 4.7,
+        "wheelchair_accessible_entrance": True,
+        "hearing_accessibility": False,
+        "visual_accessibility": False,
+        "opening_hours": "24hs",
+        "current_opening_hours": "open_now"
+    }]
+        # Check whether the result is right
+        self.assertEqual(result, json.dumps(expected_result, indent=4))
+
+    def test_filter_by_rating_1_star(self):
+        city_filter = Filter(self.paris_data)
+        target_rating = [1.0 + (x * 0.1)
+             for x in range(0, 10)]
+
+        result = city_filter.filter_by_price(target_rating)
+        expected_result = []
+        # Check whether the result is right
+        self.assertEqual(result, json.dumps(expected_result, indent=4))
+
+    def test_filter_by_rating_4_star(self):
+        city_filter = Filter(self.paris_data)
+        target_rating = [4.0 + (x * 0.1)
+             for x in range(0, 10)]
+
+        result = city_filter.filter_by_rating(target_rating)
+        expected_result = [{
+            "activity": "Eiffel Tower",
+            "price": 16,
+            "rating": 4.7,
+            "wheelchair_accessible_entrance": True,
+            "hearing_accessibility": True,
+            "visual_accessibility": True,
+            "opening_hours": "daily 9:00-00:45",
+            "current_opening_hours": "open_now"
+        },
+        {
+            "activity": "Louvre Museum",
+            "price": 15,
+            "rating": 4.8,
+            "wheelchair_accessible_entrance": True,
+            "hearing_accessibility": True,
+            "visual_accessibility": True,
+            "opening_hours": "mon, thu, sat, sun 9:00-18:00, wed, fri 9:00-21:45",
+            "current_opening_hours": "open_now"
+        },
+        {
+            "activity": "Notre-Dame Cathedral",
+            "price": 0,
+            "rating": 4.6,
+            "wheelchair_accessible_entrance": True,
+            "hearing_accessibility": False,
+            "visual_accessibility": False,
+            "opening_hours": "daily 8:00-18:45",
+            "current_opening_hours": "open_now"
+        },
+        {
+            "activity": "Montmartre",
+            "price": 0,
+            "rating": 4.5,
+            "wheelchair_accessible_entrance": False,
+            "hearing_accessibility": False,
+            "visual_accessibility": False,
+            "opening_hours": "24hs",
+            "current_opening_hours": "open_now"
+        },
+        {
+            "activity": "Seine River Cruise",
+            "price": 12,
+            "rating": 4.7,
+            "wheelchair_accessible_entrance": True,
+            "hearing_accessibility": True,
+            "visual_accessibility": True,
+            "opening_hours": "daily 9:00-23:00",
+            "current_opening_hours": "open_now"
+        },
+        {
+            "activity": "Versailles Palace",
+            "price": 20,
+            "rating": 4.6,
+            "wheelchair_accessible_entrance": True,
+            "hearing_accessibility": True,
+            "visual_accessibility": True,
+            "opening_hours": "tue-sun 9:00-18:30",
+            "current_opening_hours": "open_now"
+        },
+        {
+            "activity": "Centre Pompidou",
+            "price": 14,
+            "rating": 4.5,
+            "wheelchair_accessible_entrance": True,
+            "hearing_accessibility": True,
+            "visual_accessibility": True,
+            "opening_hours": "wed-mon 11:00-21:00",
+            "current_opening_hours": "open_now"
+        }
+    ]
+
+        # Check whether the result is right
+        self.assertEqual(result, json.dumps(expected_result, indent=4))
+
+if __name__ == '__main__':
+    unittest.main()
